017a3d187421395772c0f8503032b4273447b22f
Omnia Fathy <agxnh2020@gmail.com>
2025-09-30 21:46:43 +0000

    it is extremely ineffiecient but now we have the ability to handle multiple connections one after another

--- main.go
+++ main.go
@@ -31,50 +31,54 @@ import (
 		panic(err)
 	}
 
-
-	client_sock, client_addr, err := unix.Accept(sock)
-	if err != nil {
-		fmt.Println("Error accepting connection:", err)
-	}
-	fmt.Println("Accepted connection from:", client_addr)
-	defer unix.Close(client_sock)
-
-	buffer := make([]byte, 4096)
-	data_size, err := unix.Read(client_sock, buffer)
-	if err != nil {
-		fmt.Println("Error reading from client:", err)
-	}
-	fmt.Printf("Received data from client sized (%d): %s\n", data_size, string(buffer[:data_size]))
-
-	upstream_sock, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
-	if err != nil {
-		panic(err)
-	}
-	defer unix.Close(upstream_sock)
-
-	upstream_addr := &unix.SockaddrInet4{Port: 9090, Addr: [4]byte{127, 0, 0, 1}}
-	err = unix.Connect(upstream_sock, upstream_addr)
-	if err != nil {
-		fmt.Println("Error connecting to upstream server:", err)
-	}
-	fmt.Printf("Connected to upstream server at: %d.%d.%d.%d\n", upstream_addr.Addr[0], upstream_addr.Addr[1], upstream_addr.Addr[2], upstream_addr.Addr[3])
-
-	unix.Write(upstream_sock, buffer[:data_size])
-
-	res := make([]byte, 4096)
 	for {
 
-		res_size, err := unix.Read(upstream_sock, res)
+		client_sock, client_addr, err := unix.Accept(sock)
 		if err != nil {
-			fmt.Println("Error reading from upstream server:", err)
+			fmt.Println("Error accepting connection:", err)
 		}
-		if res_size == 0 {
-			fmt.Println("--------------------------------")
-			fmt.Println("No more data from upstream server")
-			break
+		fmt.Println("Accepted connection from:", client_addr)
+		// defer unix.Close(client_sock)
+		
+		buffer := make([]byte, 4096)
+		data_size, err := unix.Read(client_sock, buffer)
+		if err != nil {
+			fmt.Println("Error reading from client:", err)
 		}
-		fmt.Printf("Received data from upstream server sized (%d): %s\n", res_size, string(res[:res_size]))
-		unix.Write(client_sock, res[:res_size])
+		fmt.Printf("Received data from client sized (%d): %s\n", data_size, string(buffer[:data_size]))
+		
+		upstream_sock, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
+		if err != nil {
+			panic(err)
+		}
+		// defer unix.Close(upstream_sock)
+		
+		upstream_addr := &unix.SockaddrInet4{Port: 9090, Addr: [4]byte{127, 0, 0, 1}}
+		err = unix.Connect(upstream_sock, upstream_addr)
+		if err != nil {
+			fmt.Println("Error connecting to upstream server:", err)
+		}
+		fmt.Printf("Connected to upstream server at: %d.%d.%d.%d\n", upstream_addr.Addr[0], upstream_addr.Addr[1], upstream_addr.Addr[2], upstream_addr.Addr[3])
+		
+		unix.Write(upstream_sock, buffer[:data_size])
+		
+		res := make([]byte, 4096)
+		for {
+			
+			res_size, err := unix.Read(upstream_sock, res)
+			if err != nil {
+				fmt.Println("Error reading from upstream server:", err)
+			}
+			if res_size == 0 {
+				fmt.Println("--------------------------------")
+				fmt.Println("No more data from upstream server")
+				break
+			}
+			fmt.Printf("Received data from upstream server sized (%d): %s\n", res_size, string(res[:res_size]))
+			unix.Write(client_sock, res[:res_size])
+		}
+		unix.Close(upstream_sock)
+		unix.Close(client_sock)
+		
 	}
-
  }
